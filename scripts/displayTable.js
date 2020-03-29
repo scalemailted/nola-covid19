2dc8f96fd51af453a40054cfa47b79563a3b9744
Ted Holmberg <scalemailted@gmail.com>
2020-03-29 19:08:07 -0400

    added table

--- scripts/displayTable.js
+++ scripts/displayTable.js
@@ -1,29 +1,24 @@
-const renderTable = function(){
-    return  `<table class='table'>
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                    <tr>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>`
+const renderTable = function(data){
+    const table = `<table class='table table-striped'> ${renderTableHead(data[0])} ${renderTableBody(data)} </table>`;
+    document.getElementById('tableData').innerHTML = table.replace(/,/g, '');
 }
+
+const renderTableHead = function(row){
+    const tdList = [ ...Object.keys(row)];
+    return `<thead class='thead-dark'> <tr> ${tdList.map( e => renderTD(e))} </tr> </thead>`;
+}
+
+const renderTableBody = function(tableData){
+    return `<tbody>${ tableData.map( e=> renderTableRow(e))} </tbody>`;
+}
+
+const renderTableRow = function( row ){
+    const tdList = [ ...Object.values(row)]; 
+    return `<tr>${ tdList.map( e => renderTD(e)) }</tr>`;
+}
+
+const renderTD = function (datum){
+    text = datum instanceof Date ? datum.toDateString() : String(datum);
+    return `<td>${ text }</td>`
+}
+
